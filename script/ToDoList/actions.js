2301c67118823ae107191ab950139ae4002afa3e
Josh Kearl <Josh.kearl@gmail.com>
2018-04-02 13:42:46 -0600

    Fixed links in html page for toDoList

--- script/ToDoList/actions.js
+++ script/ToDoList/actions.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
 
     $(document).on("click", ".deleteBtn", function(){
         if($(".toDoRowActive")){
-            console.log(this);
-            $(this).closest(".toDoRowActive").remove();
+            $(this).closest(".toDoRowActive").slideUp("fast", function(){
+                $(this).remove();
+            })
         }
 
     });
@@ -26,18 +27,21 @@ $(document).ready(function() {
     $(".addTask").click(function () {
         var inputText = $(".taskCreator").val();
         var resetVal = $(".taskCreator").val("");
-
-        if (inputText === "") {
-            alert("Invalid input: Please enter a task");
-        } else {
-            $(".list").append("<div class='toDoRow'>\
+        var list = $(".list");
+        var appendedElement = $("<div class='toDoRow'>\
                                 <span class='checkMark'><i class='fas fa-check-square'></i></span>\
                                 <span class='listLabel'>" + inputText + "</span>\
                                 <a class=\"deleteBtn\"><i class=\"fas fa-minus-square\"></i></a>\
                          </div>");
+
+        if (inputText === "") {
+            alert("Invalid input: Please enter a task");
+        } else {
+            appendedElement.appendTo(list);
+            list.children().last().slideDown("fast");
+            list.children().last().css("display", "flex");
             resetVal;
         }
-
 
     })
 
@@ -50,5 +54,15 @@ $(document).ready(function() {
     });
 
 });
+
+function openNav (){
+    $(".sideNav").toggleClass("sideNavActive");
+}
+
+function closeNav () {
+    $(".sideNavActive").toggleClass("sideNav");
+}
+
+
 
 
