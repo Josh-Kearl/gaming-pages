76fee7559a44c6066c325bedb04a304728b969bf
Josh Kearl <Josh.kearl@gmail.com>
2018-04-26 12:12:59 -0600

    layout bug fixes

--- Golf Project/actions.js
+++ Golf Project/actions.js
@@ -16,6 +16,7 @@ function loadDoc(){
                 console.log(allcourses.courses[i].id);
                 $(".courseDropdown").append("<option value ='"+ allcourses.courses[i].id +"'>" + allcourses.courses[i].name +"</option>");
             }
+            $(".rightMenu").empty();
         }
     };
 
@@ -34,6 +35,8 @@ function getCourse(courseid){
             for (let i = 0; i < holeontees.length; i++){
                 $(".teeDropdown").append("<option value ='"+ i +"'>"+ holeontees[i].teeType +"</option>")
             }
+            $(".rightMenu").empty();
+            $(".leftMenu").empty();
         }
     };
     xhttp.open("GET", "https://uxcobra.com/golfapi/course"+ courseid +".txt", true);
@@ -41,14 +44,14 @@ function getCourse(courseid){
 }
 
 function setTee(teeindex){
-    $(".right").html("");
+    $(".rightMenu").html("");
 
     let mycourse = selcourse.data.holes;
     for(let i = 0; i < mycourse.length; i++){
-        $(".right").append("\
+        $(".rightMenu").append("\
             <div class='column' id='c" + i + "'>\
             <div class='pHeader'>" + (i + 1) + "</div>\
-            <div class='yds'" + mycourse[i].teeBoxes[teeindex].yards +"</div>\
+            <div class='yds'>" + mycourse[i].teeBoxes[teeindex].yards +" YDS</div>\
             </div>");
     }
     buildCard()
@@ -57,7 +60,7 @@ function setTee(teeindex){
 
 function buildCard (){
     for (let p =1; p<= numplayers; p++){
-        $(".left").append("<div>player"+ p + "</div>");
+        $(".leftMenu").append("<div class='players'>player " + p + "</div>");
         for(let h = 0; h < selcourse.data.holes.length; h++){
             $('#c' + h).append("<input id='p"+ p +"h "+ h +"' type='text' class='holeinput'>");
         }
