57c96e5f3f3c12adb933eb227956c007a11900c1
Josh Kearl <Josh.kearl@gmail.com>
2018-05-03 23:08:00 -0600

    Linked golf project to Ascension Web

--- script/RDice.js
+++ script/RDice.js
@@ -1,9 +1,17 @@
-let randomNumber = Math.round(Math.random() * 20);
 
 
-function rollHigh(){
-    let highNumber = (randomNumber + randomNumber) + 4;
 
-}
+
+
+
+$(".diceRoller").click(function(){
+    var x = Math.floor((Math.random() * 20) + 8);
+    if (x > 20){
+        x = 20;
+    } else {
+        console.log(x);
+    }
+
+});
 
 
